c4cb72831c3f162ae138a287e891edf080772974
Ender Zheng <crazyender@gmail.com>
2014-01-24 14:31:53 +0800

    add missing file

---
+++ FFMPEG.mono/IAudioRender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Multimedia
+{
+    public class AudioFrame
+    {
+        public IntPtr sample;
+        public int size;
+        public int rate;
+        public int bit;
+        public int channel;
+    }
+
+
+}
